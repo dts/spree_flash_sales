ead99e5b214c6fdfabc872271379f2120af86cbb
Daniel Staudigel <dstaudigel@gmail.com>
2013-11-21 10:42:20 -0800

    Drop home controller decorations.

--- app/controllers/spree/home_controller_decorator.rb
+++ app/controllers/spree/home_controller_decorator.rb
@@ -1,11 +1,11 @@
 module Spree
   HomeController.class_eval do
-    decorates_assigned :flash_sales # exposes flash_sales in view as helper method
+    # decorates_assigned :flash_sales # exposes flash_sales in view as helper method
 
-    def index
-      @flash_sales = Spree::FlashSale.live.order("position")
+    # def index
+    #   @flash_sales = Spree::FlashSale.live.order("position")
 
-      respond_with(@flash_sales)
-    end
+    #   respond_with(@flash_sales)
+    # end
   end
 end
